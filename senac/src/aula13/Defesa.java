aea06af63e85af1eaefed3c7fed238a237063bf0
SergioFelzener <49769502+SergioFelzener@users.noreply.github.com>
2020-05-22 21:00:04 -0300

    Update Defesa.java

--- senac/src/aula13/Defesa.java
+++ senac/src/aula13/Defesa.java
@@ -18,10 +18,10 @@ public class Defesa {
 			valorAtaque = ataque.ataque * 1.1 - defesa;
 		} else if (ataque.tipo == Tipo.GELO && tipo == Tipo.FOGO) {
 			valorAtaque = ataque.ataque - defesa * 1.1;
-		} else if (ataque.tipo == Tipo.AGUA && tipo == Tipo.TERRA) {
-			valorAtaque = ataque.ataque - defesa * 1.3;
 		} else if (ataque.tipo == Tipo.TERRA && tipo == Tipo.AGUA) {
-			valorAtaque = ataque.ataque - defesa * 0.9;
+			valorAtaque = ataque.ataque - defesa * 1.3;
+		} else if (ataque.tipo == Tipo.AGUA && tipo == Tipo.TERRA) {
+			valorAtaque += valorAtaque;
 		}
 
 		if (valorAtaque > 0) {
